8a8dacd4b2d39ab62456b0bf2a6ed1cdbb156a91
ART0 <18333981+0Art0@users.noreply.github.com>
2023-05-19 15:10:09 +0530

    Guidelines for tactic usage

--- TypeTheoryTalks/Demo-LogicWithTactics.lean
+++ TypeTheoryTalks/Demo-LogicWithTactics.lean
@@ -1,7 +1,15 @@
 import Mathlib.Tactic
 
 /-!
-Logic exercises using Lean's tactic mode.
+Logic exercises using Lean's tactic mode, which allows proofs to be constructed interactively.
+
+Some useful tactics are:
+- `intro`, which introduces an assumption in the context. This tactic is used when the goal is a function type.
+- `apply`, which acts on a goal of the form `⊢ Q` using a function with type `P → Q` to produce a new goal `P`.
+- `exact`, a less flexible variant of `apply` that is used to finish proofs. The tactic `exact q` completes a proof when the goal has type `⊢ Q`.
+- `cases`, to break an assumption into the relevant cases.
+- `trivial`, to prove `True`.
+- `exfalso`, to construct a function from `False`.
 -/
 
 variable (P Q R : Prop)
